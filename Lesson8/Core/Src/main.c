f35d57fefd533c26d8d24f7957619e2665bcb890
Urban <silc.urban10@gmail.com>
2026-03-08 18:23:15 +0100

    Second commit

--- Lesson8/Core/Src/main.c
+++ Lesson8/Core/Src/main.c
@@ -1,108 +1,162 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx.h"
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+  RCC ->AHBENR |= RCC_AHBENR_GPIOBEN;
 
-  /* USER CODE END SysInit */
+  // Enable clock for GPIO port B
+     RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
 
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
+     // ------------------------------------------------
+     // Configure PB6 and PB7 as Alternate Function mode
+     // (needed for I2C communication)
+     // MODER bits: 10 = Alternate Function
+     // ------------------------------------------------
 
-  /* USER CODE END 2 */
+     GPIOB->MODER &= ~GPIO_MODER_MODER6_0; // Clear bit 0 of MODER6
+     GPIOB->MODER |=  GPIO_MODER_MODER6_1; // Set bit 1 -> PB6 in Alternate Function mode
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+     GPIOB->MODER &= ~GPIO_MODER_MODER7_0; // Clear bit 0 of MODER7
+     GPIOB->MODER |=  GPIO_MODER_MODER7_1; // Set bit 1 -> PB7 in Alternate Function mode
+
+
+     // ------------------------------------------------
+     // Configure output type
+     // I2C requires open-drain outputs -> There is no need for Push-Pull because the HIGH staet is provided by outer voltage
+     // ------------------------------------------------
+
+     GPIOB->OTYPER |= GPIO_OTYPER_OT_6; // Set PB6 as open-drain
+     GPIOB->OTYPER |= GPIO_OTYPER_OT_7; // Set PB7 as open-drain
+
+
+     // ------------------------------------------------
+     // Configure output speed
+     // Lower speed is usually sufficient for I2C
+     // ------------------------------------------------
+
+     GPIOB->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR6_0; // Low speed for PB6
+     GPIOB->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR7_0; // Low speed for PB7
+
+
+     // ------------------------------------------------
+     // Disable internal pull-up and pull-down resistors
+     // (I2C typically uses external pull-up resistors)
+     // ------------------------------------------------
+
+     GPIOB->PUPDR &= ~GPIO_PUPDR_PUPDR6; // No pull-up/pull-down on PB6
+     GPIOB->PUPDR &= ~GPIO_PUPDR_PUPDR7; // No pull-up/pull-down on PB7
+
+
+     // ------------------------------------------------
+     // Configure Alternate Function for PB6 and PB7
+     // These pins are set to AF1 for I2C1 on many STM32 devices
+     // AFR[0] controls pins 0–7
+     // ------------------------------------------------
+
+     GPIOB->AFR[0] = 0b00010001000000000000000000000000;
+
+
+     // ------------------------------------------------
+     // Enable clock for I2C1 peripheral
+     // ------------------------------------------------
+
+     RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+
+
+     // ------------------------------------------------
+     // Configure I2C timing register
+     // Value depends on system clock and desired I2C speed -> The number is provided by Excel official document where you specify parameters
+     // ------------------------------------------------
+
+     I2C1->TIMINGR = (uint32_t)0x0010020A;
+
+
+     // ------------------------------------------------
+     // Enable the I2C peripheral
+     // ------------------------------------------------
+
+     I2C1->CR1 |= I2C_CR1_PE;
+
+
+     // ------------------------------------------------
+     // Set the slave device address
+     // Address is shifted left because the lowest bit is R/W
+     // ------------------------------------------------
+
+     I2C1->CR2 |= (0x1D << 1);
+
+
+     // ------------------------------------------------
+     // Select write operation
+     // RD_WRN = 0 means write
+     // ------------------------------------------------
+
+     I2C1->CR2 &= ~I2C_CR2_RD_WRN;
+
+
+     // ------------------------------------------------
+     // Set number of bytes to transfer (1 byte)
+     // Bits 16–23 of CR2 represent NBYTES
+     // ------------------------------------------------
+
+     I2C1->CR2 |= (1 << 16);
+
+
+     // ------------------------------------------------
+	 // Generate START condition
+	 // This begins the I2C transfer on the bus
+	 // ------------------------------------------------
+
+	 I2C1 ->CR2 |= I2C_CR2_START;
+
+
+	 // Wait until START condition is cleared by hardware
+	 // This means the start condition has been generated
+	 while(I2C1 ->CR2 & I2C_CR2_START);
+
+
+	 // ------------------------------------------------
+	 // Write data byte into TXDR register
+	 // Hardware will automatically transmit it
+	 // ------------------------------------------------
+
+	 I2C1 ->TXDR = 0x2C;
+
+
+	 // ------------------------------------------------
+	 // Wait until transmit register becomes empty
+	 // TXE = transmit data register empty
+	 // Means byte has been moved to shift register
+	 // ------------------------------------------------
+
+	 while(!(I2C1 ->ISR & I2C_ISR_TXE));
+
+
+	 // ------------------------------------------------
+	 // Generate STOP condition
+	 // Ends the I2C communication
+	 // ------------------------------------------------
+
+	 I2C1 ->CR2 |= I2C_CR2_STOP;
+
+
+	 // Wait until STOP condition is cleared
+	 while(I2C1 ->CR2 & I2C_CR2_STOP);
+
+
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
